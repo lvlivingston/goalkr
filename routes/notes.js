97dc9ce37105f5ec78bbeeaa90f11c61b5b60fac
lvlivingston <lvlivingston@gmail.com>
2023-10-28 13:45:58 -0500

    updated notes routes

---
+++ routes/notes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const notesCtrl = require('../controllers/notes');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
+
+// GET /notes (see all Notes)
+router.get('/notes', ensureLoggedIn, notesCtrl.index);
+// POST /okrs/:okrId/notes (create a note for an OKR)
+router.post('/okrs/:okrId/notes', ensureLoggedIn, notesCtrl.create);
+
+module.exports = router;
