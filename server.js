fdc814e87ce232b4e0c39ed35586207eade74288
lvlivingston <lvlivingston@gmail.com>
2023-11-01 20:14:15 -0500

    cleaned up comments on server.js file

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const notesRouter = require('./routes/notes');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -37,7 +36,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Add this middleware BELOW passport middleware
 app.use(function (req, res, next) {
 	res.locals.user = req.user;
   next();
@@ -47,18 +45,13 @@ app.use('/', indexRouter);
 app.use('/okrs', okrsRouter);
 app.use('/notes', notesRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
